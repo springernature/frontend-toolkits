4ad4c218717225c892ce0e4e74e8ee29ead370cb
David Paul <david.paul@springernature.com>
2020-02-23 07:53:42 +0000

    Add try catch to fontfaceobserver

--- toolkits/springer/packages/springer-webfonts/js/index.js
+++ toolkits/springer/packages/springer-webfonts/js/index.js
@@ -4,14 +4,21 @@ import FontFaceObserver from 'fontfaceobserver';
  * 1. No need to throw this.
  *	  Access to browser storage can be blocked in the browser settings and that is a valid user choice.
  *	  The try catch is required because sessionStorage will still be defined and JavaScript will error but we want it to fail silently.
+ *
+ * 2. No need to throw this.
+ *	  The try catch is required because FontFaceObserver will reject the promise with a new Error if the default timeout is exceeded. We want it to fail silently.
  */
 
 function loadFonts(config) {
 	const fonts = [].concat.apply([], config.map(font => {
 		return font.weights.map(weight => {
-			return new FontFaceObserver(font.name, {
-				weight: weight
-			});
+			try {
+				return new FontFaceObserver(font.name, {
+					weight: weight
+				});
+			} catch (error) {
+				// -- See note 2
+			}
 		});
 	}));
 
