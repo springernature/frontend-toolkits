0f6cd92bed7b1c2862212d940d6e8e5aa0428ff4
Ben Clark <hello@clarkben.com>
2020-03-04 09:44:07 +0000

    Linting pain linting pain linting pain

--- toolkits/springer/packages/springer-webfonts/__mocks__/fontfaceobserver/index.js
+++ toolkits/springer/packages/springer-webfonts/__mocks__/fontfaceobserver/index.js
@@ -1,3 +1,5 @@
 const load = jest.fn().mockImplementation(() => new Promise(resolve => resolve()));
-const mock = jest.fn().mockImplementation(() => load);
+const mock = jest.fn().mockImplementation(() => {
+	return {load: load};
+});
 export default mock;
