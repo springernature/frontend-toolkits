89a6c390d14ecbd4c8205705d363c4fa1e9b7866
Ben Clark <hello@clarkben.com>
2020-03-23 16:19:12 +0000

    Adds test descriptions

---
+++ toolkits/global/packages/global-popup/__tests__/popup.spec.js
@@ -0,0 +1,59 @@
+import PopupGroup from '../';
+
+describe('PopupGroup', () => {
+	beforeEach(() => {
+
+	});
+
+	afterEach(() => {
+
+	});
+
+	it('should build popup html including the html argument passed in to it', () => {
+
+	});
+
+	it('should open the popup above the trigger by default', () => {
+
+	});
+
+	it('should open the pop below the trigger if not enough space in viewport', () => {
+
+	});
+
+	it('should move focus back to trigger if user tabs beyond last element in popup', () => {
+
+	});
+
+	it('should close the popup if the user clicks out of it', () => {
+
+	});
+
+	it('should close the popup if the user clicks the close button', () => {
+
+	});
+
+	it('should close the popup if a keyboard user presses escape', () => {
+
+	});
+
+	it('should close the popup if a keyboard user focuses on the close button and presses space', () => {
+
+	});
+
+	it('should focus the first link in the content', () => {
+
+	});
+
+	it('should allow the focus target be overridden', () => {
+
+	});
+
+	it('should close one popup when another opens', () => {
+
+	});
+
+	it('should stop event propagation on toggle', () => {
+
+	});
+});
--- toolkits/global/packages/global-popup/js/index.js
+++ toolkits/global/packages/global-popup/js/index.js
@@ -1,3 +1,3 @@
+const PopupGroup;
 
-
-export {popup};
+export {PopupGroup};
