a714910ca31ac3fe04dea9d20f20284dff49e92b
Ben Clark <hello@clarkben.com>
2020-04-02 16:22:21 +0100

    Builds popup html

--- toolkits/global/packages/global-popup/js/index.js
+++ toolkits/global/packages/global-popup/js/index.js
@@ -1,11 +1,24 @@
 import {makeArray, getDataOptions} from '@springernature/global-context/js/helpers';
-import {Expander} from './expander';
 
 const Popup = class {
-	constructor() {
-
+	constructor(className) {
+		this._popupTriggers = makeArray(document.querySelectorAll('[data-popup]'));
+		this._componentClass = className;
+		this._closeButton = `<a href="javascript:;" class=${this._componentClass + '__close'}>Close</a>`
+		this._arrow = `<div class=${this._componentClass + '__arrow'}></div>`
 	}
 
+	_build(content) {
+		content.insertAdjacentHTML('beforeend', this._closeButton + this._arrow);
+		document.body.appendChild(content);
+	}
+
+	init() {
+		this._popupTriggers.forEach(trigger => {
+			const content = document.querySelector(`#${trigger.getAttribute('data-popup-target')}`);
+			this._build(content);
+		})
+	}
 };
 
 export {Popup};
