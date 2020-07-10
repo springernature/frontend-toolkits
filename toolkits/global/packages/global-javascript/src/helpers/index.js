7ca2faa69a581e6f73b3e8a0a12741477cd3c7ea
David Paul <david.paul@springernature.com>
2020-07-10 09:42:40 +0100

    Adds throttle

--- toolkits/global/packages/global-javascript/src/helpers/index.js
+++ toolkits/global/packages/global-javascript/src/helpers/index.js
@@ -2,8 +2,9 @@
 import {makeArray} from './util/make-array';
 import {createEvent} from './util/create-event';
 import {debounce} from './util/debounce';
+import {throttle} from './util/throttle';
 
 // Dom
 import {getDataOptions} from './dom/get-data-options';
 
-export {makeArray, createEvent, getDataOptions, debounce};
+export {makeArray, createEvent, getDataOptions, debounce, throttle};
---
+++ toolkits/global/packages/global-javascript/src/helpers/util/throttle.js
@@ -0,0 +1,18 @@
+/**
+ * @param {function} func - function to execute
+ * @param {number} wait - timeout to execute
+ * @returns {function(...[*]=)}
+ */
+
+export const throttle = (func, wait = 100) => {
+	let timer = null;
+	// not arrow function due to 'this' value
+	return function (...args) { // eslint-disable-line unicorn/prevent-abbreviations
+		if (timer === null) {
+			timer = setTimeout(() => {
+				func.apply(this, args);
+				timer = null;
+			}, wait);
+		}
+	};
+};
