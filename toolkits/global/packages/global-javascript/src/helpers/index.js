bdbe20f87cf1a337a5408d6e4ea0c1026f335118
David Paul <david.paul@springernature.com>
2020-07-10 10:34:08 +0100

    Adds getCookie

--- toolkits/global/packages/global-javascript/src/helpers/index.js
+++ toolkits/global/packages/global-javascript/src/helpers/index.js
@@ -3,8 +3,9 @@ import {makeArray} from './util/make-array';
 import {createEvent} from './util/create-event';
 import {debounce} from './util/debounce';
 import {throttle} from './util/throttle';
+import {getCookie} from './util/get-cookie';
 
 // Dom
 import {getDataOptions} from './dom/get-data-options';
 
-export {makeArray, createEvent, getDataOptions, debounce, throttle};
+export {makeArray, createEvent, debounce, throttle, getCookie, getDataOptions};
