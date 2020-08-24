4551fb9e33fe0c586d71a1dd91e6870c37198c94
David Paul <david.paul@springernature.com>
2020-08-24 14:43:34 +0100

    Adds deleteCookie and setCookie

---
+++ toolkits/global/packages/global-javascript/src/helpers/util/set-cookie.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} name - The name / key of the cookie
+ * @param {string} value - The value of the cookie
+ * @param {Object} [options] - The attribute values for the cookie. See https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie for attribute descriptions
+ * @param {string} [options.path]
+ * @param {string} [options.domain]
+ * @param {number} [options.max-age]
+ * @param {string} [options.expires]
+ * @param {string} [options.secure]
+ * @param {string} [options.samesite]
+ */
+
+export const setCookie = (name, value, options = {}) => {
+	document.cookie = encodeURIComponent(name) +
+		'=' + value +
+		(options.path ? (';path=' + options.path) : '') +
+		(options.domain ? (';domain=' + options.domain) : '') +
+		(options['max-age'] ? (';max-age=' + options['max-age']) : '') +
+		(options.expires ? (';expires=' + options.expires) : '') +
+		(options.secure ? (';secure') : '') +
+		(options.samesite ? (';samesite=' + options.samesite) : '');
+};
