f76b4731d39bd87f67a598d20cbd77a108b30f1d
Ricky Lau <ricky.lau@springernature.com>
2021-08-31 12:12:54 +0100

    fix linting errors

--- toolkits/nature/packages/nature-facet/js/enhanced-facet.js
+++ toolkits/nature/packages/nature-facet/js/enhanced-facet.js
@@ -17,7 +17,7 @@ const classNames = {
 	ELLIPSIS: 'c-facet__ellipsis',
 	CLEAR: 'c-facet__clear-selection',
 	BUTTON_CONTAINER: 'c-facet__button-container',
-	SUBMIT: 'c-facet__submit',
+	SUBMIT: 'c-facet__submit'
 };
 
 const buttonName = {
@@ -25,13 +25,13 @@ const buttonName = {
 	SELECTED: 'selected',
 	CLEAR_SELECTION: 'Clear selection',
 	SUBMIT: 'Apply filters'
-}
+};
 
 const facetTargetAttribute = 'data-facet-target';
 
-const containsCheckableInput = (element) => {
-	return !!element.querySelector('input[type=checkbox], input[type=radio]');
-}
+const containsCheckableInput = element => {
+	return Boolean(element.querySelector('input[type=checkbox], input[type=radio]'));
+};
 
 const createClearButton = (element, container) => {
 	const button = document.createElement('button');
@@ -43,27 +43,27 @@ const createClearButton = (element, container) => {
 		const checkedInputs = element.querySelectorAll('input[type=checkbox]:checked, input[type=radio]:checked');
 		let i;
 		for (i = 0; i < checkedInputs.length; ++i) {
-		  checkedInputs[i].checked = false;
+			checkedInputs[i].checked = false;
 		}
 		button.closest('form').submit();
 	});
 	container.appendChild(button);
-}
+};
 
-const createSubmitButton = (container) => {
+const createSubmitButton = container => {
 	const button = document.createElement('button');
 	button.classList.add(classNames.SUBMIT);
 	button.textContent += buttonName.SUBMIT;
 	container.appendChild(button);
-}
+};
 
-const addFormControlsInExpander = (element) => {
+const addFormControlsInExpander = element => {
 	const buttonContainer = document.createElement('div');
 	buttonContainer.classList.add(classNames.BUTTON_CONTAINER);
 	element.prepend(buttonContainer);
 	createSubmitButton(buttonContainer);
 	createClearButton(element, buttonContainer);
-}
+};
 
 const createFilterButtonName = (button, dropdown) => {
 	const checkedInput = dropdown.querySelectorAll('input[type="checkbox"]:checked, input[type="radio"]:checked');
@@ -81,7 +81,7 @@ const createFilterButtonName = (button, dropdown) => {
 		buttonSpan.textContent += buttonName.ALL;
 	}
 	button.prepend(buttonSpan);
-}
+};
 
 const enhancedFacet = () => {
 	const triggers = document.querySelectorAll(selectors.DATA_COMPONENT);
@@ -91,7 +91,7 @@ const enhancedFacet = () => {
 		return;
 	}
 
-	makeArray(triggers).forEach((trigger) => {
+	makeArray(triggers).forEach(trigger => {
 		const targetId = trigger.hasAttribute(facetTargetAttribute) && trigger.getAttribute(facetTargetAttribute);
 		const targetElement = document.querySelector(targetId);
 
