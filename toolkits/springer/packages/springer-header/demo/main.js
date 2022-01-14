fa8d3f42c137d49c356e9966d12c35791cd207e7
Heydon <heydon@heydonworks.com>
2022-01-14 10:34:12 +0000

    e -> error

--- toolkits/springer/packages/springer-header/demo/main.js
+++ toolkits/springer/packages/springer-header/demo/main.js
@@ -6,8 +6,8 @@
 	searchForm.classList.remove('c-header__search-form--no-js');
 	searchForm.hidden = true;
 
-	searchLink.addEventListener('click', e => {
-		e.preventDefault();
+	searchLink.addEventListener('click', event => {
+		event.preventDefault();
 		searchForm.classList.add('c-header__search-form--visible');
 		searchInput.focus();
 	});
