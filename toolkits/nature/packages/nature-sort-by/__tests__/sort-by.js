055456aa7ef62afa59e45efade2538e63d1399ea
Ben Clark <hello@clarkben.com>
2022-02-15 08:03:00 +0000

    Uses correct component classname prefix

--- toolkits/nature/packages/nature-sort-by/__tests__/sort-by.js
+++ toolkits/nature/packages/nature-sort-by/__tests__/sort-by.js
@@ -4,15 +4,15 @@ describe('Sort By', () => {
 	beforeEach(() => {
 		document.body.innerHTML = `
 			<form action="/" method="post">
-				<div class="t-sort-by" data-sort-by="">
-					<span class="t-sort-by__heading">Sort by:</span>
-					<input class="t-sort-by__input" data-sort-by-input="" name="order" value="relevance" id="sort-by-relevance" type="radio">
-					<label class="t-sort-by__label" for="sort-by-relevance">Relevance</label>
-					<input class="t-sort-by__input" data-sort-by-input="" name="order" value="desc" id="sort-by-desc" type="radio" checked="">
-					<label class="t-sort-by__label" for="sort-by-desc">Most recent</label>
-					<input class="t-sort-by__input" data-sort-by-input="" name="order" value="asc" id="sort-by-asc" type="radio">
-					<label class="t-sort-by__label" for="sort-by-asc">Oldest first</label>
-					<button class="t-sort-by__button" data-sort-by-button="" type="submit">Submit</button>
+				<div class="c-sort-by" data-sort-by="">
+					<span class="c-sort-by__heading">Sort by:</span>
+					<input class="c-sort-by__input" data-sort-by-input="" name="order" value="relevance" id="sort-by-relevance" type="radio">
+					<label class="c-sort-by__label" for="sort-by-relevance">Relevance</label>
+					<input class="c-sort-by__input" data-sort-by-input="" name="order" value="desc" id="sort-by-desc" type="radio" checked="">
+					<label class="c-sort-by__label" for="sort-by-desc">Most recent</label>
+					<input class="c-sort-by__input" data-sort-by-input="" name="order" value="asc" id="sort-by-asc" type="radio">
+					<label class="c-sort-by__label" for="sort-by-asc">Oldest first</label>
+					<button class="c-sort-by__button" data-sort-by-button="" type="submit">Submit</button>
 				</div>
 			</form>
 		`;
