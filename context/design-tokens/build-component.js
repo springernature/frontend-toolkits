a9f93e18a96c131c257d3735bd4370ec501d71d0
robson <stu.robson@embl.de>
2022-05-11 13:23:44 +0100

    adds component tokens build step

--- context/design-tokens/build-component.js
+++ context/design-tokens/build-component.js
@@ -22,7 +22,7 @@ function getStyleDictionaryConfig(brand, components) {
 			`${__dirname}/alias/**/*.json`
 		],
 		source: [
-			`${__dirname}/component/**/*.json`
+			`${__dirname}/components/**/*.json`
 		],
 		platforms: {
 			scssVariables: {
@@ -30,11 +30,12 @@ function getStyleDictionaryConfig(brand, components) {
 				buildPath: `${dest}/`,
 				files: components.map(component => {
 					return {
-						destination: `${component}/scss/10-settings/_${component}.variables.scss`,
+						destination: `${component}/scss/10-settings/_${brand}.variables.scss`,
 						format: 'scss/variables',
 						filter: tokenFilter(brand, component),
 						"options": {
-							"outputReferences": true
+							"outputReferences": true,
+							"showFileHeader": false
 						}
 					}
 				})
@@ -47,11 +48,46 @@ function getStyleDictionaryConfig(brand, components) {
 console.log('Build started...');
 
 ['global'].map(function (brand) {
-	let dir = `${__dirname}/component/${brand}`
+	let dir = `${__dirname}/components/${brand}`
 	const components = readdirSync(dir);
 	const brands = StyleDictionaryPackage.extend(getStyleDictionaryConfig(brand, components));
 
 	brands.buildAllPlatforms();
+
+	// let file = `${__dirname}/component/${brand}/scss/10-settings/_${brand}.variables.scss`;
+
+	components.map(component => {
+		let dir = `./toolkits/${brand}/packages/${component}/scss/10-settings`
+		const files = readdirSync(dir);
+		files.map(file => {
+			let filePath = `${dir}/${file}`
+			let content = fs.readFileSync(filePath, 'utf8');
+			let sortedContent = content.split('\n').sort().join('\n');
+			fs.writeFileSync(filePath, sortedContent);
+		})
+	});
+
+	components.map(component => {
+		let dir = `./toolkits/${brand}/packages/${component}/scss/10-settings`
+		const files = readdirSync(dir);
+		// let brand2 equal brand
+		let brand2 = brand;
+		// if brand2 equal global let brand2 equal default
+		if (brand2 === 'global') {
+			brand2 = 'default';
+		}
+
+		files.map(file => {
+			let filePath = `${dir}/${file}`
+			let content = fs.readFileSync(filePath, 'utf8');
+			let date = new Date();
+			let dateString = date.toLocaleString();
+			let newContent = `// Created: ${dateString}\n// Source: design-tokens/componenet/${brand}/${component}/${brand2}.json\n// DO NOT edit directly\n\n${content}`;
+			fs.writeFileSync(filePath, newContent);
+			// rename file to match ${brand2}.variables.scss
+			fs.renameSync(filePath, `${dir}/${brand2}.variables.scss`);
+		})
+	});
 
 	console.log('\nEnd processing');
 });
