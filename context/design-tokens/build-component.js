1441d55ed2dfdc7762ca4b483e833e867febf92c
robson <stu.robson@embl.de>
2022-09-13 16:11:14 +0100

    makes the component specific script better

--- context/design-tokens/build-component.js
+++ context/design-tokens/build-component.js
@@ -2,7 +2,16 @@ const fs = require('fs');
 const { readdirSync } = require('fs');
 const StyleDictionaryPackage = require('style-dictionary');
 
-// this will return a filtering function based on brand and component
+const componentX = process.env.npm_config_component;
+let contextX = process.env.npm_config_context;
+const brandX = componentX.split('-')[0];
+
+if (!contextX && brandX === 'global') {
+	contextX = 'default';
+} else if (!contextX && brandX !== 'global') {
+	contextX = brandX;
+}
+
 function tokenFilter(brand, component) {
 	return function (token) {
 		return (
@@ -11,39 +20,21 @@ function tokenFilter(brand, component) {
 	};
 }
 
-const componentX = process.env.npm_config_component;
-const brandX = process.env.npm_config_brand;
-
-// create a new variable called dave that is componenX before the -
-let dave = componentX.split('-')[0];
-console.log(dave);
-
 function getStyleDictionaryConfig(brand, components) {
 
 	let dest = `../../toolkits/${brandX}/packages/`;
-	console.log(dest);
-
-	let brand2 = brandX;
-	// if brand2 equal global let brand2 equal default
-	if (brand2 === 'global') {
-		brand2 = 'default';
-	}
-
-	// let source equal ${__dirname}/components/${brand2}/${componentX}/${brand2}.json
-	let sourceX = `${__dirname}/components/${brand2}/${componentX}/${brand2}.json`;
-	console.log(sourceX);
 
 	return {
 		// we want to include the default tokens first and override them with brand tokens if available
 		include: [
 			`${__dirname}/literal/default/**/*.json`,
-			`${__dirname}/literal/${brand2}/${componentX}/*.json`,
+			`${__dirname}/literal/${brandX}/${componentX}/*.json`,
 			`${__dirname}/alias/default/**/*.json`,
-			`${__dirname}/alias/${brand2}/${componentX}/*.json`
+			`${__dirname}/alias/${brandX}/${componentX}/*.json`
 		],
 		// for each brand want to generate the Sass variables from this components tokens file
 		source: [
-			`${__dirname}/components/${brandX}/${componentX}/${brand2}.json`
+			`${__dirname}/components/${brandX}/${componentX}/${contextX}.json`
 		],
 		platforms: {
 			scssVariables: {
@@ -51,7 +42,7 @@ function getStyleDictionaryConfig(brand, components) {
 				buildPath: `${dest}/`,
 				files: components.map(component => {
 					return {
-						destination: `${componentX}/scss/00-tokens/_${brand2}.tokens.scss`,
+						destination: `${componentX}/scss/00-tokens/_${contextX}.tokens.scss`,
 						format: 'scss/variables',
 						filter: tokenFilter(brand, component),
 						"options": {
@@ -65,26 +56,18 @@ function getStyleDictionaryConfig(brand, components) {
 	}
 }
 
-// run getStyleDictionaryConfig
 const styleDictionary = StyleDictionaryPackage.extend(
-	getStyleDictionaryConfig([brandX], [componentX])
+	getStyleDictionaryConfig([contextX], [componentX])
 );
 
 styleDictionary.buildAllPlatforms();
 
-let brand2 = brandX;
-// if brand2 equal global let brand2 equal default
-if (brand2 === 'global') {
-	brand2 = 'default';
-}
-
-// get the outputted file and add the date and time to the top
-const filePath = `../../toolkits/${brandX}/packages/${componentX}/scss/00-tokens/_${brand2}.tokens.scss`;
+const filePath = `../../toolkits/${brandX}/packages/${componentX}/scss/00-tokens/_${contextX}.tokens.scss`;
 const content = fs.readFileSync(filePath, 'utf8');
 const sortedContent = content.split('\n').sort().join('\n');
 
 const replacedContent = sortedContent.replace(/: \$/g, ': $t-');
 
-const GeneratedContent = `// Generated on ${new Date().toLocaleString()}\n// Source: design-tokens/components/${brandX}/${componentX}/${brand2}.json\n// DO NOT edit directly\n\n${replacedContent}`;
+const GeneratedContent = `// Generated on ${new Date().toLocaleString()}\n// Source: design-tokens/components/${brandX}/${componentX}/${contextX}.json\n// DO NOT edit directly\n\n${replacedContent}`;
 
 fs.writeFileSync(filePath, GeneratedContent, 'utf8');
