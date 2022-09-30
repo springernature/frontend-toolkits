bdd543ca31273529894188d8ac5411059a621246
Ben Clark <hello@clarkben.com>
2022-09-30 13:27:52 +0100

    Removes brand context button styles and works on js, html, scss a little more

--- toolkits/global/packages/global-ratings-survey/js/ratings-survey.js
+++ toolkits/global/packages/global-ratings-survey/js/ratings-survey.js
@@ -3,14 +3,18 @@ class RatingsSurvey {
 		this._aside = aside;
 		this._form = this._aside.querySelector('form');
 		this._formRadioFieldset = this._form.querySelector('fieldset');
+		this._formRadios = this._form.querySelectorAll('[data-ratings-survey="radio"]');
 		this._formRadioLabels = this._form.querySelectorAll('[data-ratings-survey="radio"]+label');
+		this._submitButton = this._form.querySelector('button[type="submit"]');
 		this._submitMessage = this._form.querySelector('[data-ratings-survey="submit-message"]');
 		this._permissibleUserJourneys = ['getPublished', 'contentDiscovery'];
 		this._bindEvents();
 	}
 
-	_getRadioValue(label) {
-		return document.querySelector(`#${label.getAttribute('for')}`).value;
+	_getCheckedRadioValue() {
+		Array.from(this._formRadios).find(element => {
+			return element.checked;
+		});
 	}
 
 	_getUserJourneys() {
@@ -29,13 +33,12 @@ class RatingsSurvey {
 		return false;
 	}
 
-	_dispatchDataLayerEvent(label) {
+	_dispatchDataLayerEvent(radioValue) {
 		if (!window.dataLayer) {
 			console.error('Attempt to send Global Ratings Survey event failed. window.dataLayer does not exist.');
 			return;
 		}
 		const userJourneys = this._getUserJourneys();
-		const radioValue = this._getRadioValue(label);
 		if (userJourneys && radioValue) {
 			window.dataLayer.push({
 				// this event name corresponds to the GTM trigger setup for this solution
@@ -47,17 +50,33 @@ class RatingsSurvey {
 	}
 
 	_displayMessage() {
+		this._submitButton.classList.add('u-hide');
 		this._formRadioFieldset.classList.add('u-hide');
 		this._submitMessage.classList.remove('u-hide');
 	}
 
 	_bindEvents() {
 		this._formRadioLabels.forEach(label => {
-			label.addEventListener('click', () => {
-				this._dispatchDataLayerEvent(label);
-				this._displayMessage();
+			label.addEventListener('keydown', event => {
+				if (event.key === 'Enter') {
+					event.preventDefault();
+					event.stopPropagation();
+				}
 			});
 		});
+		this._form.addEventListener('submit', event => {
+			event.preventDefault();
+		});
+		// ['click', 'keydown'].forEach(eventType => {
+		// 	this._submitButton.addEventListener(eventType, event => {
+		// 		if (/Enter|Space/.test(event.key) || event.type === 'click') {
+		// 			event.preventDefault();
+		// 			event.stopPropagation();
+		// 			this._dispatchDataLayerEvent(this._getCheckedRadioValue());
+		// 			this._displayMessage();
+		// 		}
+		// 	});
+		// });
 	}
 }
 
