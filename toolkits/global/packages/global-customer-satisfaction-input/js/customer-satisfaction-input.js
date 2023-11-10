5e21d565feda2cd0f7f8fc1b93552aa85f123180
Roman Seidelsohn <rseidelsohn@gmail.com>
2023-11-10 09:01:46 +0100

    Add line breaks and add userJourneys and additionalInfo to survey link
    - Wrap long lines
    - Add userJourneys and additionalInfo parameters and their values to the
      survey link, so that they can be combined with the survey input users
      provide for us

--- toolkits/global/packages/global-customer-satisfaction-input/js/customer-satisfaction-input.js
+++ toolkits/global/packages/global-customer-satisfaction-input/js/customer-satisfaction-input.js
@@ -3,12 +3,32 @@ class CustomerSatisfactionInput {
 		this._aside = aside;
 		this._form = this._aside.querySelector('form');
 		this._formRadioFieldset = this._form.querySelector('fieldset');
-		this._formRadios = Array.from(this._form.querySelectorAll('[data-customer-satisfaction-input="radio"]'));
-		this._surveyLink = this._form.querySelector('[data-customer-satisfaction-input="survey-link"]');
+		this._formRadios = Array.from(
+			this._form.querySelectorAll('[data-customer-satisfaction-input="radio"]')
+		);
+		this._surveyLink = this._form.querySelector(
+			'[data-customer-satisfaction-input="survey-link"]'
+		);
 		this._setSurveyLinkHref();
 		this._submitButton = this._form.querySelector('button[type="submit"]');
-		this._submitMessage = this._form.querySelector('[data-customer-satisfaction-input="submit-message"]');
-		this._permissibleUserJourneys = ['get prepared to publish', 'get published', 'discover relevant scholarly content', 'manage my editorial work', 'manage my peer reviews', 'promote my work', 'evaluate the performance of scholarly work', 'manage an apc', 'buy something', 'access what i am entitled to', 'librarian get the information i need', 'librarian assess the performance and use of my portfolio', 'librarian buy something'];
+		this._submitMessage = this._form.querySelector(
+			'[data-customer-satisfaction-input="submit-message"]'
+		);
+		this._permissibleUserJourneys = [
+			'get prepared to publish',
+			'get published',
+			'discover relevant scholarly content',
+			'manage my editorial work',
+			'manage my peer reviews',
+			'promote my work',
+			'evaluate the performance of scholarly work',
+			'manage an apc',
+			'buy something',
+			'access what i am entitled to',
+			'librarian get the information i need',
+			'librarian assess the performance and use of my portfolio',
+			'librarian buy something'
+		];
 		this._bindEvents();
 	}
 
@@ -28,7 +48,10 @@ class CustomerSatisfactionInput {
 
 	_setSurveyLinkHref() {
 		if (this._surveyLink) {
-			this._surveyLink.href = this._surveyLink.href + '?location=' + window.location.href.split('?')[0];
+			this._surveyLink.href =
+				this._surveyLink.href +
+				'?location=' +
+				window.location.href.split('?')[0];
 		}
 	}
 
@@ -36,28 +59,57 @@ class CustomerSatisfactionInput {
 		this._updateSurveyLinkQueryString('responseRating', rating);
 	}
 
+	_setSurveyLinkUserJourneys(userJourneys) {
+		if (userJourneys) {
+			this._updateSurveyLinkQueryString('userJourneys', userJourneys);
+		}
+	}
+
+	_setSurveyLinkAdditionalInfo(additionalInfo) {
+		if (additionalInfo) {
+			this._updateSurveyLinkQueryString('additionalInfo', additionalInfo);
+		}
+	}
+
 	_getUserJourneys() {
 		if (!this._aside.dataset.customerSatisfactionInputUserJourneys) {
-			console.error('Attempt to send Global Customer Satisfaction Input event failed. Value not found for User Journeys.');
+			console.error(
+				'Attempt to send Global Customer Satisfaction Input event failed. Value not found for User Journeys.'
+			);
 			return;
 		}
-		const userJourneyStrings = this._aside.dataset.customerSatisfactionInputUserJourneys.split(',');
-		const sanitisedUserJourneyStrings = userJourneyStrings.map(string => string.trim().toLowerCase());
-		const containsPermissibleUserJourneys = sanitisedUserJourneyStrings.every(string => {
-			return this._permissibleUserJourneys.includes(string);
-		});
+
+		const userJourneyStrings =
+			this._aside.dataset.customerSatisfactionInputUserJourneys.split(',');
+		const sanitisedUserJourneyStrings = userJourneyStrings.map(string =>
+			string.trim().toLowerCase()
+		);
+		const containsPermissibleUserJourneys = sanitisedUserJourneyStrings.every(
+			string => {
+				return this._permissibleUserJourneys.includes(string);
+			}
+		);
+
 		if (containsPermissibleUserJourneys) {
 			return sanitisedUserJourneyStrings.join(',');
 		}
-		console.error('Attempt to send Global Customer Satisfaction Input event failed. One or more of the user journeys provided are not permissible values.');
+
+		console.error(
+			'Attempt to send Global Customer Satisfaction Input event failed. One or more of the user journeys provided are not permissible values.'
+		);
+
 		return false;
 	}
 
 	_dispatchDataLayerEvent(radioValue) {
 		if (!window.dataLayer) {
-			console.error('Attempt to send Global Customer Satisfaction Input event failed. window.dataLayer does not exist.');
+			console.error(
+				'Attempt to send Global Customer Satisfaction Input event failed. window.dataLayer does not exist.'
+			);
+
 			return;
 		}
+
 		const userJourneys = this._getUserJourneys();
 		if (userJourneys && radioValue) {
 			window.dataLayer.push({
@@ -65,7 +117,8 @@ class CustomerSatisfactionInput {
 				event: 'survey.track',
 				userJourneys: userJourneys,
 				radioValue: radioValue,
-				additionalInfo: this._aside.dataset.customerSatisfactionInputAdditionalInfo || null
+				additionalInfo:
+					this._aside.dataset.customerSatisfactionInputAdditionalInfo || null
 			});
 		}
 	}
@@ -81,22 +134,33 @@ class CustomerSatisfactionInput {
 			event.preventDefault();
 			event.stopPropagation();
 		});
-		['click', 'keydown'].forEach(eventType => {
+
+		const eventTypes = ['click', 'keydown'];
+
+		// ['click', 'keydown'].forEach((eventType) => {
+		for (const eventType of eventTypes) {
 			this._submitButton.addEventListener(eventType, event => {
 				if (/Enter|Space/.test(event.key) || event.type === 'click') {
 					event.preventDefault();
 					event.stopPropagation();
 					const validForm = this._validateForm();
+
 					if (validForm) {
-						this._setSurveyLinkResponseRating((this._getCheckedRadioValue()));
+						this._setSurveyLinkResponseRating(this._getCheckedRadioValue());
+						this._setSurveyLinkUserJourneys(this._getUserJourneys());
+						this._setSurveyLinkAdditionalInfo(
+							this._aside.dataset.customerSatisfactionInputAdditionalInfo
+						);
 						this._dispatchDataLayerEvent(this._getCheckedRadioValue());
 						this._displayMessage();
+
 						return;
 					}
+
 					this._displayError();
 				}
 			});
-		});
+		}
 	}
 
 	_validateForm() {
@@ -106,7 +170,9 @@ class CustomerSatisfactionInput {
 	}
 
 	_displayError() {
-		if (this._form.classList.contains('c-customer-satisfaction-input--show-error')) {
+		if (
+			this._form.classList.contains('c-customer-satisfaction-input--show-error')
+		) {
 			return;
 		}
 		this._form.classList.add('c-customer-satisfaction-input--show-error');
